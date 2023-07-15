b47d234d949b9ac56470ce29a95b9097424c374b
brkagithub <mbrkic16@gmail.com>
2023-07-15 13:16:03 +0200

    basic langchain example

--- src/env.mjs
+++ src/env.mjs
@@ -8,6 +8,7 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
+    // OPENAI_API_KEY: z.string(),
   },
 
   /**
@@ -16,7 +17,7 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_OPENAI_API_KEY: z.string().min(1),
   },
 
   /**
@@ -25,6 +26,8 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    // OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,35 @@
-import Head from "next/head";
-import Link from "next/link";
 import { api } from "~/utils/api";
 import { OpenAI } from "langchain/llms/openai";
+import { PromptTemplate } from "langchain/prompts";
+import { ConversationChain } from "langchain/chains";
+import { env } from "../env.mjs";
+
+const template = "What is a good name for a company that makes {product}?";
+const prompt = new PromptTemplate({
+  template: template,
+  inputVariables: ["product"],
+});
 
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const model = new OpenAI({
+    openAIApiKey: env.NEXT_PUBLIC_OPENAI_API_KEY,
+    temperature: 0.9,
+  });
+  const chain = new ConversationChain({ llm: model, prompt: prompt });
 
   return (
     <>
-      <div>hello {hello.data?.greeting}</div>
+      <div
+        onClick={async () => {
+          console.log("here");
+          const res = await chain.call({ product: "black socks" });
+
+          console.log(res);
+        }}
+      >
+        hello {hello.data?.greeting}
+      </div>
     </>
   );
 }
